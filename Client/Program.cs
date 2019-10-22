eff6d9c512fed0699f304879bfbe13b957a43598
Clemens Schneider <clemens.schneider@posteo.de>
2019-10-22 12:50:05 +0200

    removed comments

--- Client/Program.cs
+++ Client/Program.cs
@@ -17,33 +17,18 @@ namespace TcpIp
         {
             try
             {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
                 int port = 13000;
                 TcpClient client = new TcpClient(server, port);
 
-                // Translate the passed message into ASCII and store it as a Byte array.
                 byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
 
                 NetworkStream stream = client.GetStream();
 
-                // Send the message to the connected TcpServer. 
                 stream.Write(data, 0, data.Length);
 
-                // Receive the TcpServer.response.
-
-                // Buffer to store the response bytes.
                 data = new byte[256];
-
-                // String to store the response ASCII representation.
                 string responseData = string.Empty;
 
-                // Read the first batch of the TcpServer response bytes.
                 int bytes = stream.Read(data, 0, data.Length);
                 responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
                 Console.WriteLine("Received: {0}", responseData);
